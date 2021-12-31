eae7759af4122421a825716049d27f2a1f8cb6c6
FAXec <xecfahim.ahmed@gmail.com>
2021-12-31 18:05:52 +0000

    updated app.js

--- js/app.js
+++ js/app.js
@@ -89,44 +89,6 @@ window.onclick = function(event) {
   }
 }
 
-// <-----Lightbox ----->
-
-var slideIndex = 1;
-showSlide(slideIndex);
-
-function changeSlide(n) {
-	showSlide(slideIndex += n);
-}
-
-function toSlide(n) {
-	showSlide(slideIndex = n);
-}
-
-function showSlide(n) {
-
-  const slides = document.getElementsByClassName('image');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
-
-  if (n > slides.length) {
-    slideIndex = 1;	
-  }
-  
-  if (n < 1) {
-  	slideIndex = slides.length;
-  }
-
-  for (let i = 0; i < slides.length; i++) {    
-      slides[i].style.display = "none";
-      // console.log(slides)
-  }
-  
-  for (let i = 0; i < modalPreviews.length; i++) {
-      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-  }
-  
-  // slides[slideIndex - 1].style.display = 'block';
-  // modalPreviews[slideIndex - 1].className += ' active';
-}
 
 
 
